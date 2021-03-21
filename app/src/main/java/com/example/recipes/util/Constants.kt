eefbb6ae79a8a646ea937c4fa849ba6f838a5283
Tushar <paltushar150@gmail.com>
2021-03-21 13:40:28 +0530

    updated row layout of recipes and added recyclerview in fragment_recipes and connect it with the api

--- app/src/main/java/com/example/recipes/util/Constants.kt
+++ app/src/main/java/com/example/recipes/util/Constants.kt
@@ -6,6 +6,14 @@ class Constants {
 
         const val BASE_URL = "https://api.spoonacular.com"
         const val API_KEY = "a251cf8d2e73437994da41a13b02cbe9"
+
+        // API Query Keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
     }
 
 }
